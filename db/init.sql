6dbf987345a49c7f461e8818ccc82ca4cd671db1
JavaRip <up941226@myport.ac.uk>
2023-04-05 20:20:04 +0100

    add postgresdb

--- db/init.sql
+++ db/init.sql
@@ -5,13 +5,15 @@ CREATE TABLE Deck (
 
 CREATE TABLE Card (
   id SERIAL PRIMARY KEY,
-  deckId INTEGER
-);
-
-CREATE TABLE Review (
-  ID SERIAL PRIMARY KEY,
   deckId INTEGER,
   front VARCHAR,
   back VARCHAR
+);
+
+CREATE TABLE Review (
+  id SERIAL PRIMARY KEY,
+  cardId INTEGER,
+  ts INTEGER,
+  correct Boolean
 );
 
